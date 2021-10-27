d577c60b70e7adb680dceeb433061e1715984ba2
unknown <dattajunior@gmail.com>
2021-10-27 19:51:30 +0700

    fixed hero responsitivty for large devices and updated Utility UI

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
     <div>
       <img alt="Vue logo" class="background-image" src="/img/backgrounds/beach_long.png" v-bind:style="'height:' + windowHeight + 'px'">
       <div style="background-color:#d4c579;height:150px"/>
-      <img src="/img/sharks/shark_dark_blue_reverse.png" class="shark shark2"/>
-      <img src="/img/sharks/shark_dark_blue.png" class="shark shark1"/>
-      <img src="/img/sharks/shark_dark_blue.png" class="shark shark3"/>
-      <img src="/img/sharks/shark_dark_blue_reverse.png" class="shark shark4"/>
+      <img src="/img/sharks/3.png" class="shark shark2"/>
+      <img src="/img/sharks/4.png" class="shark shark1"/>
+      <img src="/img/sharks/2.png" class="shark shark3"/>
+      <img src="/img/sharks/16.png" class="shark shark4"/>
+      <img src="/img/sharks/3.png" class="shark shark5"/>
+      <img src="/img/sharks/7.png" class="shark shark6"/>
       <img id="cloud1" src="img/props/cloud_transparent.png" class="cloud cloud1" />
       <img id="cloud2" src="img/props/cloud_transparent.png" class="cloud cloud2" />
       <img id="cloud3" src="img/props/cloud_transparent.png" class="cloud cloud3" />
@@ -14,33 +16,38 @@
       <img id="cloud5" src="img/props/cloud_transparent.png" class="cloud cloud5" />
       <img id="cloud6" src="img/props/cloud_transparent.png" class="cloud cloud6" />
       
-      <img src="img/props/coconut_tree_transparent.png" class="coconut-tree" />
+      <img src="img/props/palm_tree.png" class="palm-tree" />
       <div class="text-container" >
         <div class="d-flex justify-content-center">
         <img src="/img/logos/logo1.png" class="logo" />
         </div>
         <div class="d-flex justify-content-center mt-4">
-          <label class="montserrat" style="font-size: 22px;"><b>Worlds first, zero utility, negative value, collectable NFT sharks.</b></label>
+          <label class="montserrat" style="font-size: 22px;"><b>Zero utility, negative value, collectable NFT sharks.</b></label>
         </div>
         <div class="d-flex justify-content-center mt-4">
-          <a href="https://twitter.com/SharkpunksNFT" style="text-decoration:none;">
-            <div class="hero-button">
+          <div class="hero-button">
+            <a href="https://twitter.com/SharkpunksNFT" style="text-decoration:none;">
+
               <div class="d-flex justify-content-center px-3">
                 <!-- scroll down to how to join -->
                 <img class="my-auto" src="/img/logos/twitter_white.png" style="height:24px">
+                
                 <label class="m-2 open-sans" style="font-size: 20px;color:white">Twitter</label> 
               </div>
-            </div>
-          </a>
-          <a href="https://discord.com/invite/oh-geez" style="text-decoration:none">
+            </a>
+
+          </div>
             <div class="hero-button mx-4">
-              <div class="d-flex justify-content-center px-3">
-                <!-- scroll down to how to join -->
-                <img class="my-auto" src="/img/logos/discord_white.png" style="height:20px">
-                <label class="m-2 open-sans" style="font-size: 20px;color:white">Discord</label> 
-              </div>
+              <a href="https://discord.com/invite/oh-geez" style="text-decoration:none">
+
+                <div class="d-flex justify-content-center px-3">
+                  <!-- scroll down to how to join -->
+                  <img class="my-auto" src="/img/logos/discord_white.png" style="height:20px">
+                  <label class="m-2 open-sans" style="font-size: 20px;color:white">Discord</label> 
+                </div>
+              </a>
+
             </div>
-          </a>
         </div>
       </div>
 
@@ -52,52 +59,59 @@
         <div class="paragraph-container my-auto mx-5" style="text-align:left">
           <p class="montserrat" style="font-size:24px;font-weight:bold">Future utilization of sharkpunks NFTs may include, but are not limited to:</p>
           <br>
-          <p class="mx-3 open-sans">- Decorative painting in the basement of your mother</p>
-          <p class="mx-3 open-sans">- Comparison of phallus size</p>
-          <p class="mx-3 open-sans">- Companion as you contemplate your existential crisis</p>
+          <div class="bullet-point-box py-4 my-3">
+          <p class="mx-3 open-sans bullet-points my-auto">Decorative painting in the basement of your mother</p>
+          </div>
+          <div class="bullet-point-box py-4 my-3">
+          <p class="mx-3 open-sans bullet-points my-auto">Comparison of phallus size</p>
+          </div>
+          <div class="bullet-point-box py-4 my-3">
+          <p class="mx-3 open-sans bullet-points my-auto">Companion as you contemplate your existential crisis</p>
+          </div>
         </div>
         <div class="mx-5 my-auto">
-          <img v-bind:src="shark" />
+          <img v-bind:src="shark" style="width:300px"/>
         </div>
       </div>
     </div>
-    <div class="d-flex justify-content-center my-5">
-      <div class="my-auto">
-        <div class="mx-5">
-          <div class="d-flex justify-content-center my-5">
-              <img class="my-auto mx-3" src="img/logos/shoyu.png" style="width:50px;height:50px">
-              <p class="inter my-auto" style="font-size:36px">SHOYU</p>
-            </div>
-          <p class="open-sans my-5" style="font-size:22px">Sharkpunks, one of the first NFTs to be listed on Shoyu</p>
-          </div>
-        </div>
-    </div>
     <br>
-    <div class="d-flex justify-content-center my-5">
-      <div class="d-flex justify-content-end my-auto">
-        <div class="mx-5">
-          <div class="my-auto mx-5">
-            <label class="big-number open-sans">1088</label>
+    <div style="height:700px">
+      <div class="d-flex justify-content-center m-5">
+        <h1 class="open-sans"><b>Roadmap</b></h1>
+      </div>
+      <div style="position:absolute;height:600px;background-color:red" v-bind:style="'left:' + screenCenter + 'px'">
+        <div style="position:relative;height:600px">
+          <div class="roadmap-bar" style=""/>
+          <div class="roadmap-point-checked" style="top:40px" />
+          <div class="roadmap-point"  style="top:220px" />
+          <div class="roadmap-point" style="top:400px" />
+          <div class="roadmap-box"  style="top:0px">
+            <div class="d-flex justify-content-center">
+              <h2><b>Q4 2021</b></h2>
+            </div>
+            <label class="montserrat">
+              First Sharkpunks minted for LevX Dao genesis members and eligible raffle winners
+            </label>
           </div>
-          <div class="my-auto">
-            <label class="number-description montserrat">total in existence</label>
+          <div class="roadmap-box2" style="top:180px">
+            <div class="d-flex justify-content-center">
+              <h2><b>Q1 2022</b></h2>
+            </div>
+            <label class=" montserrat">
+              Remaining supply minted up to 10,000 Sharkpunks
+            </label>
           </div>
-        </div>
-        <div class="mx-5">
-          <div class="my-auto mx-5">
-            <label class="big-number open-sans">750</label>
+          <div class="roadmap-box"  style="top:360px">
+            <div class="d-flex justify-content-center">
+              <h2><b>Q1 2022</b></h2>
+            </div>
+            <label class=" montserrat">
+              Sharkpunks listed as one of the first collectibles in shoyu
+            </label>
           </div>
-          <div class="my-auto">
-            <label class="number-description montserrat">airdropped via raffle</label>
-          </div>
-        </div>
-        <div class="mx-5">
-          <div class="my-auto mx-5">
-            <label class="big-number open-sans">333</label>
-          </div>
-          <div class="my-auto">
-            <label class="number-description montserrat">to first discord members</label>
-          </div>
+
+
+          
         </div>
       </div>
     </div>
@@ -105,8 +119,12 @@
         <div class="my-auto">
           <h1 class="open-sans" ><b>Join our community</b></h1>
           <div class="d-flex justify-content-center my-5">
-            <img src="img/logos/twitter.png" class="mx-4" style="width:80px"/>
-            <img src="img/logos/discord.png" class="mx-4" style="width:80px"/>
+            <a href="https://twitter.com/SharkpunksNFT" style="text-decoration:none;">
+              <img src="img/logos/twitter.png" class="mx-4" style="width:80px"/>
+            </a>
+            <a href="https://discord.com/invite/oh-geez" style="text-decoration:none">
+              <img src="img/logos/discord.png" class="mx-4" style="width:80px"/>
+            </a>
           </div>
         </div>
     </div>
@@ -135,11 +153,19 @@ export default {
 
     }
   },
+  computed : {
+    screenCenter: function () {
+      return this.windowWidth / 2
+    }
+  },
   created() {
     window.console.log(this.screenWidth)
     window.console.log(this.windowHeight)
     this.loopImage()
-
+    window.addEventListener('resize', () => {
+      this.windowHeight = window.innerHeight
+      this.windowWidth = window.outerWidth
+    })
   },
   mounted(){
 
@@ -161,6 +187,9 @@ export default {
     // cloud1.style.right
     // window.console.log(cloud1.style)
 
+  },
+  unmounted() {
+    window.removeEventListener('resize')
   },
   methods: {
     loopImage(){
@@ -240,55 +269,130 @@ export default {
   left: 0px;
 }
 
+/* small */
+
+@media only screen and (max-width: 600px) {
+  .palm-tree {
+    left: 0px;
+
+  }
+
+  .roadmap-box2 {
+  position:absolute;
+  font-weight:bold;
+  font-size:18px;
+  width:250px;
+  left:100px
+  } 
+}
+
+  
+
+/* medium */
+
+@media only screen and (min-width: 601px) and (max-width: 1299px){
+  .palm-tree {
+    left: 150px;
+  }
+}
+
+/* large */
+
+@media only screen and (min-width: 1300px) {
+  .palm-tree {
+    left: 100px;
+    height: 50%
+
+
+  }
+
+
+.palm-tree {
+  position:absolute;
+    bottom: 0px;
+}
+
+  
+.roadmap-box2 {
+  position:absolute;
+  font-weight:bold;
+  font-size:18px;
+  width:250px;
+  right: 70px;
+}
+
 .shark {
   position: absolute
 }
 
 .shark1 {
-  width: 100px;
-  right: 100px;
-  bottom: 50px;
+  width: 250px;
+  right: 50px;
+  bottom: -90px;
+  
 }
 
 .shark2 {
-  width: 70px;
-  right: 250px;
-  bottom: 100px
+  width: 250px;
+  right: 300px;
+  bottom: -90px;
+  transform: scaleX(-1);
+
 }
 .shark3 {
-  width: 40px;
-  right: 400px;
-  bottom: 200px;
-  animation-name:shark-bobble;
+  width: 80px;
+  right: 380px;
+  bottom: 23%;
+  animation-name:shark3-bobble;
   animation-duration: 2s;
   animation-iteration-count: infinite;
   animation-timing-function:linear;
 }
 
 .shark4 {
-  width: 40px;
-  right: 450px;
-  bottom: 200px;
-  animation-name:shark-bobble;
+  width: 80px;
+  right: 470px;
+  bottom: 23%;
+  animation-name:shark4-bobble;
   animation-duration: 2s;
   animation-iteration-count: infinite;
   animation-timing-function:linear;
   animation-delay: 1s;
+  transform: scaleX(-1);
 }
 
-@keyframes shark-bobble {
-  0% {transform: translateY(0%);}
-  50% {transform: translateY(-20%);}
+.shark5 {
+  width: 120px;
+  right: 600px;
+  bottom: 50px;
 }
+
+.shark6 {
+  width: 120px;
+  right: 750px;
+  bottom: 50px;
+  transform: scaleX(-1)
+}
+
+@keyframes shark3-bobble {
+  0% {transform: translateY(0%)}
+  50% {transform: translateY(-20%)}
+}
+
+@keyframes shark4-bobble {
+  0% {transform: translateY(0%) scaleX(-1)}
+  50% {transform: translateY(-20%) scaleX(-1)}
+}
+
 
 
 
 .ocean-solid {
   position:absolute;
   right:350px;
-  bottom:180px;
+  bottom:23.9%;
   width: 200px;
-  height:40px
+  height:45px
 }
 
 .cloud {
@@ -297,7 +401,7 @@ export default {
 
 .cloud1 {
   right: 200px;
-  top: 100px;
+  top: 17%;
   width: 100px;
   animation-name:cloud1-animation;
   animation-duration: 45s;
@@ -310,7 +414,7 @@ export default {
 
 .cloud2 {
   right: 300px;
-  top: 250px;
+  top: 30%;
   width: 200px;
   animation-name:cloud2-animation;
   animation-duration: 30s;
@@ -322,7 +426,7 @@ export default {
 
 .cloud3 {
   right: 500px;
-  top: 50px;
+  top: 8%;
   width: 300px;
   animation-name:cloud3-animation;
   animation-duration: 15s;
@@ -335,7 +439,7 @@ export default {
 
 .cloud4 {
   right: 100px;
-  top: 400px;
+  top: 50%;
   width: 50px;
   animation-name:cloud4-animation;
   animation-duration: 60s;
@@ -347,7 +451,7 @@ export default {
 
 .cloud5 {
   right: 600px;
-  top: 400px;
+  top: 50%;
   width: 60px;
   animation-name:cloud5-animation;
   animation-duration: 55s;
@@ -360,7 +464,7 @@ export default {
 
 .cloud6 {
   right: 750px;
-  top: 330px;
+  top: 50%;
   width: 120px;
   animation-name:cloud6-animation;
   animation-iteration-count: infinite;
@@ -385,15 +489,11 @@ export default {
 
 .text-container {
   position:absolute;
-  top: 130px;
+  top: 10%;
   left:100px
 }
 
-.coconut-tree {
-  position:absolute;
-  left: 150px;
-  bottom: 50px;
-}
+
 
 
 .ocean-color {
@@ -410,7 +510,69 @@ export default {
 }
 
 .paragraph-container p{
-  font-size: 18px
+  font-size: 18pxx  
 
 }
+
+.roadmap-bar {
+  position:absolute;
+  top: 40px;
+  width:4px;
+  height:360px;
+  background-color:black;
+  left: -2px;
+}
+
+.roadmap-point {
+  position:absolute;
+  width:20px;
+  height:20px;
+  border-radius:50%;
+  background-color:white;
+  left: -10px;
+  border-style: solid;
+  border-width:3px;
+  border-color:black;
+
+} 
+.roadmap-point-checked {
+  position:absolute;
+  width:20px;
+  height:20px;
+  border-radius:50%;
+  background-color:black;
+  left: -10px;
+
+
+} 
+
+
+.roadmap-box {
+  position:absolute;
+  font-weight:bold;
+  font-size:18px;
+  width:250px;
+  left: 50px;
+
+
+}
+
+.bullet-points {
+  font-size: 18px;
+  color:#8C808F
+}
+
+.bullet-point-box {
+  background-color:white;
+  box-shadow: 0px 0px 5px 2px #c9c1bb;
+  border-radius: 5px
+
+}
+
+
+
+}
+
+
+
 </style>
